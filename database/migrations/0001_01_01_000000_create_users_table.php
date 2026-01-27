c9ce4822d0ddecc8b1b7a6778ddc44aa412ece15
alericser <alericser@alu.edu.gva.es>
2026-01-27 19:56:29 +0100

    Hago migraciones

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,27 +13,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('nickname')->unique();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('mail')->unique();
+            $table->string('image');
+            $table->bigInteger('level')->default(0);
+            $table->bigIncrements('general_xp')->default(0);
+            $table->boolean('isAdmin')->default(0);
+            $table->token();
             $table->timestamps();
-        });
-
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-        });
-
-        Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
-            $table->string('ip_address', 45)->nullable();
-            $table->text('user_agent')->nullable();
-            $table->longText('payload');
-            $table->integer('last_activity')->index();
         });
     }
 
